2d8a1dc670401513ccf0eb109bc066fc6d6b32e0
Alex <alexanderson1993@users.noreply.github.com>
2018-11-03 20:17:56 -0600

    Working on adding styles and making it look sliiick!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Routes from './routes' 
+import Routes from "./routes";
+import Layout from "./layout";
+import "bootstrap/scss/bootstrap.scss";
 import "./styles.css";
 
-const App  = () => <Routes />
+const App = () => (
+  <Layout>
+    <Routes />
+  </Layout>
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Link } from "@reach/router";
+import Profile from "./profile";
+import { ReactComponent as Logo } from "./logo.svg";
+import "./style.scss";
 
-const Layout = (props) => {
-    // Layout includes
-    // - Global navigation
-    // - User Profile link
-    // - Frame
-    // It should also be changable, in case we 
-    // feel like mixing it up.
-    return <div className="layout-line">
+const Layout = ({ children }) => {
+  // Layout includes
+  // - Global navigation
+  // - User Profile link
+  // - Frame
+  // It should also be changable, in case we
+  // feel like mixing it up.
+  const alertColor = "5";
+  return (
+    <div className={`layout-line alertColor${alertColor}`}>
+      <header>
+        <Link to="/">
+          <div className="logo">
+            <Logo />
+          </div>
+        </Link>
+        <menu>
+          <Link to="about">About</Link>
+          <Link to="centers">Centers</Link>
+          <Link to="badges">Badges</Link>
+        </menu>
+        <Profile />
+      </header>
+      <main>{children}</main>
     </div>
-}
+  );
+};
 export default Layout;
