58bb13847142b818c6608db7fe7ad250a29dcf2e
unknown <tarronlane@hotmail.com>
2019-02-08 19:57:46 -0700

    added claim mutation

--- functions/graphql/models/BadgeAssignment.js
+++ functions/graphql/models/BadgeAssignment.js
@@ -1,41 +1,67 @@
 const { firestore } = require("../connectors/firebase");
+const { ApolloError } = require('apollo-server-express'); 
 // =============================================================================
 // Class for Querying/Mutating Assignments
 // =============================================================================
 
 module.exports = class BadgeAssignment {
-  static async createAssignment(badgeId, flightRecordId) {
-    const assignmentData = await firestore()
-      .collection("badgeAssignments")
-      .add({
-        badgeId,
-        flightRecordId,
-        dateAwarded: new Date(),
-        token: generateFriendlyToken()
-      });
+    constructor({ badgeId, flightRecordId, dateAwarded, id}) {
+        this.id = id;
+        this.badgeId = badgeId;
+        this.flightRecordId = flightRecordId;
+        this.dateAwarded = dateAwarded;
+    }
 
-    return assignmentData;
-  }
+    static async createAssignment(badgeId, flightRecordId) {
+        const assignmentData = await firestore()
+            .collection("badgeAssignments")
+            .add({
+                badgeId,
+                flightRecordId,
+                dateAwarded: new Date(),
+                token: generateFriendlyToken()
+            });
 
-  static async getAssignments(badgeId) {}
+        return assignmentData;
+    }
 
-  static async getAssignment(id) {}
+    /**
+     * Retrieve a badge assignment from the database, based on the token
+     * @param string token 
+     * @returns BadgeAssignment
+     */
+    static async getAssignment(token) {
+        let badgeAssignments = await firestore()
+            .collection("badgeAssignments")
+            .where("token", "==", token)
+            .get()
+            .then(found => (found.docs.map(ba => ({ ...ba.data(), id: ba.id }))));
+        
+        // There should only ever be one badge assignment for a token, but just in case there are conflicts
+        if (badgeAssignments.length > 1) {
+            throw new ApolloError('Collision found in badge assignment tokens. Offending token: \'' + token) 
+                + '\'. Offending assignments: [\'' 
+                + badgeAssignments.reduce((a, b, i) => ((i == 0) ? b.id : a +  '\', \'' + b.id), '') 
+                + '\']';
+        } 
 
-  constructor({ badgeId, flightRecordId, dateAwarded }) {
-    this.badgeId = badgeId;
-    this.flightRecordId = flightRecordId;
-    this.dateAwarded = dateAwarded;
-  }
+        return new BadgeAssignment({ ...badgeAssignments[0] });
+    }
 
-  async delete() {}
+    /**
+     * Delete the loaded badge assignment (usally after having claimed it to a user)
+     */
+    async delete() { 
+
+    }
 };
 
 function generateFriendlyToken() {
-  var length = 8,
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-    retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
+    var length = 10,
+        charset = "abcdefghijklmnopqrstuvwxyz0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
 }
--- functions/graphql/schema/badge.js
+++ functions/graphql/schema/badge.js
@@ -1,7 +1,7 @@
 const {
-  gql,
-  ForbiddenError,
-  UserInputError
+    gql,
+    ForbiddenError,
+    UserInputError
 } = require("apollo-server-express");
 const getCenter = require("../helpers/getCenter");
 const { Badge, User, BadgeAssignment } = require("../models");
@@ -43,7 +43,18 @@ module.exports.schema = gql`
     badgeRename(badgeId: ID!, name: String!): Badge
     badgeChangeDescription(badgeId: ID!, description: String!): Badge
     badgeChangeImage(badgeId: ID!, image: Upload!): Badge
-    badgeAssign(badgeId: ID!, flightId: ID!, userId: ID): Badge
+    badgeAssign(badgeId: ID!, flightId: ID!, userId: ID): Badge @auth(requires: [center, director])
+    badgeClaim(token: String!): ClaimResult @auth(requires: [authenticated])
+  }
+
+  type ClaimResult {
+      isSuccess: Boolean
+      badge: Badge
+      failureType: CLAIM_FAILURE
+  }
+
+  enum CLAIM_FAILURE {
+    ALREADY_CLAIMED_BY_USER
   }
 
   extend type User {
@@ -59,91 +70,101 @@ module.exports.schema = gql`
 // the functionality in this file. These will be
 // deep merged with the other resolvers.
 module.exports.resolver = {
-  Query: {
-    badges: async (rootQuery, { type, centerId }, context) => {
-      let centerIdValue = centerId;
-      if (!centerIdValue) {
-        try {
-          const center = await getCenter(context.user);
-          if (!center) {
-            throw new UserInputError('"centerId" is a required parameter.');
-          }
-          centerIdValue = center.id;
-        } catch (err) {
-          null;
+    Query: {
+        badges: async (rootQuery, { type, centerId }, context) => {
+            let centerIdValue = centerId;
+            if (!centerIdValue) {
+                try {
+                    const center = await getCenter(context.user);
+                    if (!center) {
+                        throw new UserInputError('"centerId" is a required parameter.');
+                    }
+                    centerIdValue = center.id;
+                } catch (err) {
+                    null;
+                }
+            }
+            return Badge.getBadges(type, centerIdValue);
+        },
+        badge: (rootQuery, { id }, context) => {
+            return Badge.getBadge(id);
         }
-      }
-      return Badge.getBadges(type, centerIdValue);
     },
-    badge: (rootQuery, { id }, context) => {
-      return Badge.getBadge(id);
-    }
-  },
-  Mutation: {
-    badgeCreate: async (rootQuery, { badge }, context) => {
-      const center = await getCenter(context.user);
-      return Badge.createBadge(badge, center.id);
-    },
-    badgeRemove: async (rootQuery, { badgeId }, context) => {
-      const center = await getCenter(context.user);
-      const badge = await Badge.getBadge(badgeId);
-      if (badge.centerId !== center.id)
-        throw new ForbiddenError("Cannot delete a badge you do not own.");
-      return badge.delete();
-    },
-    badgeRename: async (rootQuery, { badgeId, name }, context) => {
-      const center = await getCenter(context.user);
-      const badge = await Badge.getBadge(badgeId);
-      if (badge.centerId !== center.id)
-        throw new ForbiddenError("Cannot edit a badge you do not own.");
-      return badge.rename(name);
-    },
-    badgeChangeDescription: async (
-      rootQuery,
-      { badgeId, description },
-      context
-    ) => {
-      const center = await getCenter(context.user);
-      const badge = await Badge.getBadge(badgeId);
-      if (badge.centerId !== center.id)
-        throw new ForbiddenError("Cannot edit a badge you do not own.");
-      return badge.changeDescription(description);
-    },
-    badgeChangeImage: async (rootQuery, { badgeId, image }, context) => {
-      const center = await getCenter(context.user);
-      const badge = await Badge.getBadge(badgeId);
-      if (badge.centerId !== center.id)
-        throw new ForbiddenError("Cannot edit a badge you do not own.");
-      return badge.changeImage(image);
-    },
-    badgeAssign: async (rootQuery, { badgeId, flightId, userId }, context) => {
-      // Either assign the badge directly, or create an assignment object
-      let user = null;
-      let badge = Badge.getBadge(badgeId);
-      if (typeof userId !== "undefined") {
-        user = await User.getUserById(userId);
-      }
+    Mutation: {
+        badgeCreate: async (rootQuery, { badge }, context) => {
+            const center = await getCenter(context.user);
+            return Badge.createBadge(badge, center.id);
+        },
+        badgeRemove: async (rootQuery, { badgeId }, context) => {
+            const center = await getCenter(context.user);
+            const badge = await Badge.getBadge(badgeId);
+            if (badge.centerId !== center.id)
+                throw new ForbiddenError("Cannot delete a badge you do not own.");
+            return badge.delete();
+        },
+        badgeRename: async (rootQuery, { badgeId, name }, context) => {
+            const center = await getCenter(context.user);
+            const badge = await Badge.getBadge(badgeId);
+            if (badge.centerId !== center.id)
+                throw new ForbiddenError("Cannot edit a badge you do not own.");
+            return badge.rename(name);
+        },
+        badgeChangeDescription: async (
+            rootQuery,
+            { badgeId, description },
+            context
+        ) => {
+            const center = await getCenter(context.user);
+            const badge = await Badge.getBadge(badgeId);
+            if (badge.centerId !== center.id)
+                throw new ForbiddenError("Cannot edit a badge you do not own.");
+            return badge.changeDescription(description);
+        },
+        badgeChangeImage: async (rootQuery, { badgeId, image }, context) => {
+            const center = await getCenter(context.user);
+            const badge = await Badge.getBadge(badgeId);
+            if (badge.centerId !== center.id)
+                throw new ForbiddenError("Cannot edit a badge you do not own.");
+            return badge.changeImage(image);
+        },
+        badgeAssign: async (rootQuery, { badgeId, flightId, userId }, context) => {
+            // Either assign the badge directly, or create an assignment object
+            let user = null;
+            let badge = Badge.getBadge(badgeId);
+            if (typeof userId !== "undefined") {
+                user = await User.getUserById(userId);
+            }
 
-      if (user === null) {
-        // If user does not exist, create assignment object
-        await BadgeAssignment.createAssignment(badgeId, flightId);
-        return badge;
-      } else {
-        // If user does exist, append to that user's badges array
-        await User.assignBadge(user.id, badgeId, flightId);
-        return badge;
-      }
+            if (user === null) {
+                // If user does not exist, create assignment object
+                await BadgeAssignment.createAssignment(badgeId, flightId);
+                return badge;
+            } else {
+                // If user does exist, append to that user's badges array
+                await User.assignBadge(user.id, badgeMeta);
+                return badge;
+            }
+        },
+        /**
+         * Assign a Badge Assignment object from the database to a user, then remove
+         * the badge assignment from the database
+         */
+        badgeClaim: async (rootQuery, { token }, context) => {
+            let badgeAssignment = await BadgeAssignment.getAssignment(token);
+            delete badgeAssignment['id'];
+            let { isSuccess, failureType } = await User.assignBadge(context.user.id, badgeAssignment);
+            return { isSuccess: isSuccess, failureType: failureType };
+        }
+    },
+    User: {
+        badges: (user, { type }, context) => {
+            return Promise.all(user.badges.map(badgeMeta => Badge.getBadge(badgeMeta.badgeId)))
+                .then(badges => {
+                    return badges.filter(badge => badge.type === (typeof (type) !== 'undefined' ? type : badge.type)); // If type was submitted, only return badges matching the type
+                });
+        }
+    },
+    Center: {
+        badges: (user, { type }, context) => { }
     }
-  },
-  User: {
-    badges: (user, { type }, context) => {
-      return Promise.all(user.badges.map(badgeMeta => Badge.getBadge(badgeMeta.badgeId)))
-        .then(badges => {
-            return badges.filter(badge => badge.type === (typeof(type) !== 'undefined' ? type : badge.type)); // If type was submitted, only return badges matching the type
-        });
-    }
-  },
-  Center: {
-    badges: (user, { type }, context) => {}
-  }
 };
