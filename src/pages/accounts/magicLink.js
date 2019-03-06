d5382aa27c434227d641e357b5a36d2509991ce8
Alex Anderson <alexanderson1993@gmail.com>
2019-03-06 15:21:07 -0700

    Fixes an issue with signing in with the magic link.

--- src/pages/accounts/magicLink.js
+++ src/pages/accounts/magicLink.js
@@ -1,27 +1,56 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  useRef,
+  useCallback
+} from "react";
 import AuthContext from "../../helpers/authContext";
 import { Blockquote, Loading } from "@arwes/arwes";
 import { Words, Link, Navigator, Content } from "../../components";
 
-const MagicLink = ({ location, navigate }) => {
-  const context = useContext(AuthContext);
-  const { completeMagicLinkSignin } = context;
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const to = localStorage.getItem("postLoginPath") || "/";
+// Hook
+function useWhyDidYouUpdate(name, props) {
+  // Get a mutable ref object where we can store props ...
+  // ... for comparison next time this hook runs.
+  const previousProps = useRef();
 
-    completeMagicLinkSignin(location.href)
-      .then(() => navigate(to))
-      .catch(error => setError(error));
-  }, [completeMagicLinkSignin, location.href, navigate]);
-  return error ? (
-    <Blockquote layer="alert">
-      <Words>{error.message}</Words>
-      <p>
-        Try <Link to="/accounts/login">logging in</Link> again.
-      </p>
-    </Blockquote>
-  ) : (
+  useEffect(() => {
+    if (previousProps.current) {
+      // Get all keys from previous and current props
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      // Use this object to keep track of changed props
+      const changesObj = {};
+      // Iterate through keys
+      allKeys.forEach(key => {
+        // If previous is different from current
+        if (previousProps.current[key] !== props[key]) {
+          // Add to changesObj
+          changesObj[key] = {
+            from: previousProps.current[key],
+            to: props[key]
+          };
+        }
+      });
+
+      // If changesObj not empty then output to console
+      if (Object.keys(changesObj).length) {
+        console.log("[why-did-you-update]", name, changesObj);
+      }
+    }
+
+    // Finally update previousProps with current props for next hook call
+    previousProps.current = props;
+  });
+}
+
+const MagicLink = props => {
+  const { completeSignin, href } = props;
+  useWhyDidYouUpdate("MagicLink", props);
+  useEffect(() => {
+    completeSignin(href);
+  }, [completeSignin, href]);
+  return (
     <Content>
       <h1>
         <Words animate>Logging In...</Words>
@@ -31,9 +60,32 @@ const MagicLink = ({ location, navigate }) => {
   );
 };
 
-const MagicLinkWithNavigator = props => (
-  <Navigator>
-    {navigate => <MagicLink navigate={navigate} {...props} />}
-  </Navigator>
-);
+const NavComp = ({ navigate, location, ...props }) => {
+  const context = useContext(AuthContext);
+  const { completeMagicLinkSignin } = context;
+  const [error, setError] = useState(null);
+  const to = localStorage.getItem("postLoginPath") || "/";
+  const completeSignin = useRef(href => {
+    completeMagicLinkSignin(href)
+      .then(() => navigate(to))
+      .catch(error => setError(error));
+  });
+  return error ? (
+    <Blockquote layer="alert">
+      <Words>{error.message}</Words>
+      <p>
+        Try <Link to="/accounts/login">logging in</Link> again.
+      </p>
+    </Blockquote>
+  ) : (
+    <MagicLink href={location.href} completeSignin={completeSignin.current} />
+  );
+};
+const MagicLinkWithNavigator = props => {
+  return (
+    <Navigator>
+      {navigate => <NavComp navigate={navigate} {...props} />}
+    </Navigator>
+  );
+};
 export default MagicLinkWithNavigator;
---
+++ src/pages/redeem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Content } from "@arwes/arwes";
+
+const Redeem = () => {
+  return (
+    <Content>
+      <h1>Redeem Flight</h1>
+    </Content>
+  );
+};
+export default Redeem;
